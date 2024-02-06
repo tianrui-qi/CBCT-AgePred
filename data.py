af0cc74c2bd95efbdf8dae4f782f87d3dbf71b19
Tianrui Qi <tianrui.qi@icloud.com>
2024-02-05 22:34:37 -0500

    fix age reading error or unit8

--- data.py
+++ data.py
@@ -7,19 +7,17 @@ import torchio as tio
 import tifffile     # read and save tiff file
 import os
 import re
-
 import tqdm         # progress bar
 from typing import List, Tuple
 
 
 class Patients(Dataset):
     def __init__(
-        self, cbct_fold: str, tiff_fold: str, info_fold: str, 
+        self, tiff_fold: str, info_fold: str, 
         min_HU: int, max_HU: int, degrees: float, translation: int, std: float
     ) -> None:
         super(Patients, self).__init__()
         # path
-        self.cbct_fold = cbct_fold
         self.tiff_fold = tiff_fold
         self.info_fold = info_fold
         # normalization
@@ -46,12 +44,15 @@ class Patients(Dataset):
 
     def _getInfoList(self) -> Tensor:
         info_list = [None] * len(self.name_list)
-        for i in tqdm.tqdm(range(len(self.name_list)), desc="_getInfoList"):
+        for i in tqdm.tqdm(
+            range(len(self.name_list)), desc="_getInfoList", leave=False
+        ):
             info_path = os.path.join(self.info_fold, self.name_list[i] + ".txt")
-            with open(info_path, 'r') as file:
+            with open(info_path, 'r', encoding='utf-8') as file:
                 for line in file:
                     # split
                     split: List[str] = line.strip().split()
+                    if len(split) < 2: continue
                     # age
                     y = re.search(r'(\d+)年', split[1])
                     m = re.search(r'(\d+)月', split[1])
@@ -60,7 +61,8 @@ class Patients(Dataset):
                     m = int(m.group(1)) if m else 0
                     d = int(d.group(1)) if d else 0
                     age: float = (y * 365.25 + m * 30.5 + d) / 365.25
-                    if info_list[i] is None: info_list[i] = age
+                    if info_list[i] is None: 
+                        info_list[i] = age
                     if info_list[i] != age: print(info_path, "age miss match")
         return torch.tensor(info_list).float()
 
@@ -95,6 +97,9 @@ class Patients(Dataset):
             tiff = tiff[528-416:528, 100:100+416, 112:112+416]
         elif tiff.shape == (576, 768, 768):
             tiff = tiff[576-416:576,  50: 50+416, 176:176+416]
+
+        # reshape to (1, 416, 416, 416), i.e., (B, D, H, W)
+        tiff = tiff.reshape(1, 416, 416, 416)
 
         return torch.clip(tiff, 0, 1), self.info_list[index]
 
