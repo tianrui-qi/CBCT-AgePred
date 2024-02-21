0402d0ab32a1a399d9073530e67cf0b6f5aa781f
Tianrui Qi <tianrui.qi@icloud.com>
2024-02-20 21:26:27 -0500

    change model from vit to vit + mae

---
+++ src/model.py
@@ -0,0 +1,5 @@
+from src.vit_pytorch.vit_3d import ViT
+#from src.model.cct_3d import CCT
+from src.vit_pytorch.mae import MAE
+
+__all__ = ["ViT", "MAE"]
--- src/vit_pytorch/vit_with_patch_dropout.py
+++ src/vit_pytorch/vit_with_patch_dropout.py
@@ -1,17 +1,36 @@
 import torch
-import torch.nn as nn
+from torch import nn
 
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
-__all__ = ["ViT"]
+# helpers
 
-
-def _pair(t):
+def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
+# classes
 
-class _FeedForward(nn.Module):
+class PatchDropout(nn.Module):
+    def __init__(self, prob):
+        super().__init__()
+        assert 0 <= prob < 1.
+        self.prob = prob
+
+    def forward(self, x):
+        if not self.training or self.prob == 0.:
+            return x
+
+        b, n, _, device = *x.shape, x.device
+
+        batch_indices = torch.arange(b, device = device)
+        batch_indices = rearrange(batch_indices, '... -> ... 1')
+        num_patches_keep = max(1, int(n * (1 - self.prob)))
+        patch_indices_keep = torch.randn(b, n, device = device).topk(num_patches_keep, dim = -1).indices
+
+        return x[batch_indices, patch_indices_keep]
+
+class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
         self.net = nn.Sequential(
@@ -25,8 +44,7 @@ class _FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-
-class _Attention(nn.Module):
+class Attention(nn.Module):
     def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
         super().__init__()
         inner_dim = dim_head *  heads
@@ -49,9 +67,7 @@ class _Attention(nn.Module):
     def forward(self, x):
         x = self.norm(x)
         qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(
-            lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv
-        )
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
 
         dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
 
@@ -62,17 +78,14 @@ class _Attention(nn.Module):
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
 
-
-class _Transformer(nn.Module):
+class Transformer(nn.Module):
     def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
         super().__init__()
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                _Attention(
-                    dim, heads = heads, dim_head = dim_head, dropout = dropout
-                ),
-                _FeedForward(dim, mlp_dim, dropout = dropout)
+                Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout),
+                FeedForward(dim, mlp_dim, dropout = dropout)
             ]))
     def forward(self, x):
         for attn, ff in self.layers:
@@ -80,42 +93,30 @@ class _Transformer(nn.Module):
             x = ff(x) + x
         return x
 
-
 class ViT(nn.Module):
-    def __init__(
-        self, *, image_size, image_patch_size, frames, frame_patch_size, 
-        num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, 
-        dim_head = 64, dropout = 0., emb_dropout = 0.
-    ):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., patch_dropout = 0.25):
         super().__init__()
-        image_height, image_width = _pair(image_size)
-        patch_height, patch_width = _pair(image_patch_size)
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
 
         assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
-        assert frames % frame_patch_size == 0, 'Frames must be divisible by frame patch size'
 
-        num_patches = (image_height // patch_height) * (image_width // patch_width) * (frames // frame_patch_size)
-        patch_dim = channels * patch_height * patch_width * frame_patch_size
-
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.to_patch_embedding = nn.Sequential(
-            Rearrange(
-                'b c (f pf) (h p1) (w p2) -> b (f h w) (p1 p2 pf c)', 
-                p1 = patch_height, p2 = patch_width, pf = frame_patch_size
-            ),
-            nn.LayerNorm(patch_dim),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
             nn.Linear(patch_dim, dim),
-            nn.LayerNorm(dim),
         )
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.pos_embedding = nn.Parameter(torch.randn(num_patches, dim))
         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+
+        self.patch_dropout = PatchDropout(patch_dropout)
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = _Transformer(
-            dim, depth, heads, dim_head, mlp_dim, dropout
-        )
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -125,13 +126,17 @@ class ViT(nn.Module):
             nn.Linear(dim, num_classes)
         )
 
-    def forward(self, video):
-        x = self.to_patch_embedding(video)
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
         b, n, _ = x.shape
 
+        x += self.pos_embedding
+
+        x = self.patch_dropout(x)
+
         cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+
         x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)
 
         x = self.transformer(x)
