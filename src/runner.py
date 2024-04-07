bd497bee4ccc6235f9b63c9f9730a83f20540561
Tianrui Qi <tianrui.qi@icloud.com>
2024-04-06 23:38:45 -0400

    combine pretrain and finetune runner

--- src/runner.py
+++ src/runner.py
@@ -8,36 +8,42 @@ import os
 import src.data, src.model
 
 
-__all__ = ["PretrainTrainer", "FineturnTrainer"]
+__all__ = ["Trainer"]
 
 
-class PretrainTrainer:
+class Trainer:
     def __init__(
         self, max_epoch: int, accumu_steps: int, 
         batch_size: int, num_workers: int,
         version: str | None, save_top_k: int, 
         ckpt_load_path: str | None, ckpt_load_lr: bool,
-        trainset: src.data.PretrainDataset, 
-        validset: src.data.PretrainDataset,
-        model: src.model.PretrainModel,
+        trainset: src.data.PretrainDataset | src.data.FinetuneDataset, 
+        validset: src.data.PretrainDataset | src.data.FinetuneDataset,
+        model: src.model.PretrainModel | src.model.FinetuneModel,
     ) -> None:
         # data
         self.trainloader = torch.utils.data.DataLoader(
             trainset, batch_size=batch_size, num_workers=num_workers, 
-            persistent_workers=True, pin_memory=True,
+            persistent_workers=True, #pin_memory=True,
         )
         self.validloader = torch.utils.data.DataLoader(
             validset, batch_size=batch_size, num_workers=num_workers, 
-            persistent_workers=True, pin_memory=True,
+            persistent_workers=True, #pin_memory=True,
         )
         # model
-        if ckpt_load_lr:
+        if ckpt_load_lr or not ckpt_load_path:
             self.model = model
-        else:
+        elif isinstance(model, src.model.PretrainModel):
             self.model = src.model.PretrainModel.load_from_checkpoint(
                 ckpt_load_path, strict=False,
                 lr=model.lr,
                 vit_kwargs=model.vit_kwargs, mae_kwargs=model.mae_kwargs,
+            )
+        elif isinstance(model, src.model.FinetuneModel):
+            self.model = src.model.FinetuneModel.load_from_checkpoint(
+                ckpt_load_path, strict=False,
+                lr=model.lr, 
+                unet_kwargs=model.unet_kwargs,
             )
         # recoder
         self.ckpt_load_path = ckpt_load_path
@@ -67,11 +73,3 @@ class PretrainTrainer:
             val_dataloaders=self.validloader,
             ckpt_path=self.ckpt_load_path if self.ckpt_load_lr else None,
         )
-
-
-class FineturnTrainer:
-    def __init__(self) -> None:
-        pass
-
-    def fit(self) -> None:
-        pass
